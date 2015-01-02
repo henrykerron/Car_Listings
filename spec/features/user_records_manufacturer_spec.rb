72f32b66658c01e3bc96e0cda6cab6de27e30b8f
henrykerron <henrykerron@gmail.com>
2015-01-02 11:37:28 -0500

    Added validations and error messages for manufacturer fields

--- spec/features/user_records_manufacturer_spec.rb
+++ spec/features/user_records_manufacturer_spec.rb
@@ -9,7 +9,7 @@ feature "user records new manufacturer", %q(
 
 	[x] I must specify a manufacturer name and country.
 	[x] If I specify the required information, the manufacturer is recorded and I am redirected to the index of manufacturers
-	[] If I do not specify the required information, I am presented with errors.
+	[x] If I do not specify the required information, I am presented with errors.
 
 	) do
 
@@ -28,6 +28,14 @@ feature "user records new manufacturer", %q(
 
 		expect(page).to have_content manufacturer.name
 		expect(page).to have_content manufacturer.country
+	end
 
+	scenario "user gets errors if required information is not filled in" do
+		visit new_manufacturer_path
+
+		click_button "Add Manufacturer"
+
+		expect(page).to have_content("Name can't be blank")
+		expect(page).to have_content("Country can't be blank")
 	end
 end
